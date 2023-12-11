fa4ddb3e679bfdde915a5902925b0fee5f3f97e0
asadbek <abduvaliyevasad23@gmail.com>
2023-12-11 16:34:25 +0500

    handler

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -5,6 +5,7 @@ namespace App\Telegram;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Role;
+use App\Models\TelegramAccounts;
 use App\Models\User;
 use DefStudio\Telegraph\Enums\ChatActions;
 use Illuminate\Support\Str;
@@ -26,7 +27,7 @@ class Handler  extends WebhookHandler
     
     public function start():void
     {
-        $this->typing();
+        // $this->typing();
         $user = $this->user( $this->chat->chat_id);
      
         if($user == null){
@@ -38,8 +39,17 @@ class Handler  extends WebhookHandler
       
         
     }
+
+    protected function handleUnknownCommand(Stringable $text):void
+    {
+        if($text->value() == '/start'){
+            $this->reply('command exited');
+        }
+    }
+
     public function register(): void
     {
+        $this->setpage('register');
         $this->typing();
         $this->chat
             ->html('Please send your phone number for registration: +998 ** *** ** **')
@@ -57,29 +67,67 @@ class Handler  extends WebhookHandler
     {
         $this->typing();
         $contact = $this->message?->contact()?->phoneNumber() ?? null;
-        $phone_number_validation_regex = "/^998([378]{2}|(9[013-57-9]))\d{7}$/";
-   
-        if ($text->value() === '⬅️ Главное меню') {
+
+        if($text->value() === '⬅️ Главное меню') {
+         
             $this->menu();
-        } elseif (preg_match($phone_number_validation_regex, str_replace('+', '', preg_replace('/\s+/', '', $text)))) {
-            $this->user_store($text,  $this->chat->chat_id);
-            $this->menu();
-        } elseif($contact  !== null || $contact  !== []){
-            $this->user_store($contact,  $this->chat->chat_id);
-            $this->menu();
+        } else {
+            switch($this->getPage())
+            {   
+                case 'register':
+                    if($contact  !== null || $contact  !== []){
+                        $this->user_store($contact,  $this->chat->chat_id);
+                        $this->menu();
+                    }
+                    elseif($text->value() !== '')
+                    {
+                        if ( $this->regexPhoneNumber($text->value())) {
+                            $this->user_store($text,  $this->chat->chat_id);
+                            $this->menu();
+                        } else {
+                            $this->chat->message('Natog`ri formatda yozdingiz')->send();
+                        }
+                    }
+                    break;
+                case 'comment': 
+                    if($text->value() !== '')
+                    {   
+                        $this->chat->message("😊 Спасибо за комментарий")->send();
+                        $this->menu();
+                    }
+                    break;
+                case 'setting':
+                    if($contact  !== null || $contact  !== []){
+                        // $this->updatePhone($contact);
+                    }
+                    elseif($text->value() !== '')
+                    {
+                        $this->chat->message($text->value())->send();
+                        if ($this->regexPhoneNumber($text->value())) {
+                            // $this->updatePhone($text->value());
+                        } else {
+                            $this->chat->message('Natog`ri formatda yozdingiz')->send();
+                        }
+                    }
+                    break;
+                // default:
+                //     $this->menu();
+            }
         }
+    
     }
     
 
     public function menu($bol = false, $edit = false):void
     {
         $this->typing();
+        $this->setpage('menu');
         $inlineKeyboard = Keyboard::make()
         ->row([
             Button::make('🛒 Начать заказ')->action('order'),
         ])
         ->row([
-            Button::make('ℹ️ О нас')->action('about'),
+            Button::make('И о нас')->action('about'),
             Button::make('🛍 Мои заказы')->action('my_order'),
         ])
         ->row([
@@ -105,7 +153,6 @@ class Handler  extends WebhookHandler
             ->keyboard($inlineKeyboard)
             ->send();
         }
-        
         
     }
 
@@ -155,10 +202,25 @@ class Handler  extends WebhookHandler
     public function category(): void 
     {
         $category_id = $this->data->get('category_id');
-        $product = Product::where('category_id', $category_id)
+        $products = Product::where('category_id', $category_id)
                    ->select('id', 'title')
                    ->get();
-        $this->chat->message(json_encode($product))->send();
+        if($products !== []){
+            foreach ($products as $product) {
+                $keybord[] =   Button::make($product->title)->action('products')->param('product_id', $product->id);
+            }
+            $keybord[] =  Button::make('⬅️ Назад')->action('product_back');
+            $this->chat->edit($this->messageId)->message('Выберит продукт')
+            ->keyboard(Keyboard::make()->buttons($keybord)->chunk(2))->send(); 
+        } else {
+            $this->chat->message('Пустой')->send();
+        }
+        
+    }
+
+    public function product_back()
+    {
+        $this->order();
     }
 
     public function menus():void
@@ -167,10 +229,10 @@ class Handler  extends WebhookHandler
         $this->menu(true , true);
     }
     
-    public function setting():void
+    public function setting($boll = false)
     {
-        
        $this->typing();
+       $this->setpage('setting');
         // Telegraph::deleteMessage($this->messageId)->send();
         $inlineKeyboard = Keyboard::make()
         ->row([
@@ -182,16 +244,41 @@ class Handler  extends WebhookHandler
         $user = $this->user( $this->chat->chat_id);
        
         $phone =$user->phone ;
-       
+       if($boll == false){
         $this->chat->edit($this->messageId)->html(
             "<b> Телефон:</b> $user->phone \n\n Выберите одно из следующих "
         )
         ->keyboard($inlineKeyboard)
         ->send();
+       } else {
+           $this->chat->html( "<b> Телефон:</b> $user->phone \n\n Выберите одно из следующих ")
+           ->keyboard($inlineKeyboard)
+           ->send();
+       }
+        
     }
-
-
-    private function user($chatId)
+    
+    public function comment()
+    {
+        Telegraph::deleteMessage($this->messageId)->send();
+        $this->setpage('comment');
+        $this->chat->message("написать комментарий")->send();
+    }
+    
+    public function phone()
+    {
+        Telegraph::deleteMessage($this->messageId)->send();
+        $this->chat
+        ->html('Смена номера: +998 ** *** ** ** ')
+        ->replyKeyboard(
+            ReplyKeyboard::make()
+                ->buttons([ReplyButton::make('Отправить номер телефона')->requestContact(true)])
+                ->resize(true)
+                ->inputPlaceholder("Phone number ...")
+        )
+        ->send();
+    }
+    private function user()
     {
         $user = User::with('telegramAccounts')
         ->whereHas('telegramAccounts', function($query)
@@ -201,18 +288,51 @@ class Handler  extends WebhookHandler
       
         return $user ?? null;
     }
+    
+
+    private function setpage(string $page):void
+    {
+       $account = TelegramAccounts::where('chat_id', $this->chat->chat_id)->update([
+            'telegram_page' => $page
+       ]);
+    }
+
+    private function regexPhoneNumber($number)
+    {
+        $phone_number_validation_regex = "/^998([378]{2}|(9[013-57-9]))\d{7}$/";
+        $boolen =  preg_match($phone_number_validation_regex, str_replace('+', '', preg_replace('/\s+/', '', $number)));
+        return $boolen;
+    }
+    
+    private function getPage()
+    {
+        $account = TelegramAccounts::where('chat_id', $this->chat->chat_id)->first()['telegram_page'];
+
+        return $account;
+    }
+
+    public function updatePhone($phone):void
+    {   
+        $user = $this->user();
+        User::find($user->id)->update([
+            'phone' => $phone
+        ]);
+        $replyKeyboard = ReplyKeyboard::make()
+        ->row([
+            ReplyButton::make('⬅️ Главное меню'),
+        ])->resize(true);
+        $messagid = Telegraph::message('Номер изменился')
+                ->replyKeyboard($replyKeyboard)
+                ->send();
+        $this->setting(true);
+    }
 
     private function typing()
     {
         $this->chat->action(ChatActions::TYPING)->send();
     }
 
-    protected function handleUnknownCommand(Stringable $text):void
-    {
-        if($text->value() == '/start'){
-            $this->reply('command exited');
-        }
-    }
+   
 }
 
 
